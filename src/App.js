460da4872a6e1149afbebe963c0a00590ec266fe
Aiperi Eldar <aiperieldar.dev@gmail.com>
2022-04-28 16:12:12 -0400

    added new feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Expenses from './components/Expenses/Expenses'
-
+import NewExpense from './components/NewExpense/NewExpense'
 
 function App() {
   const expenses = [
@@ -27,8 +27,14 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('q')
+    console.log(expense)
+  }
+
   return (
     <div>
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses items={expenses}/>
     </div>
   );
